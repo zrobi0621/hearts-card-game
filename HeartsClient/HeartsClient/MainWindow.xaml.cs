690f7761abce0177e2969b786afcbb82cf3d6ef0
zrobi0621 <33635244+zrobi0621@users.noreply.github.com>
2019-12-07 20:21:54 +0100

    v

--- HeartsClient/HeartsClient/MainWindow.xaml.cs
+++ HeartsClient/HeartsClient/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace HeartsClient
 {
@@ -22,45 +23,46 @@ namespace HeartsClient
     /// </summary>
     public partial class MainWindow : Window
     {
-        public Socket client;
-
-        // Data buffer for incoming data.  
+        public Socket client;        
         public byte[] bytes = new byte[1024];
 
-        public int bytesRec;
-
-        public int whoIamB = 0;
-        public int isP2Connected = 0;
-        public int game = 0;
-        public string receivedMsg = "";
+        int whoIam = 0;
+        bool isMyTurn = false;
+        bool quitGame = false;
+        bool canIWait = false;
 
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();            
         }
 
         public void ConnectClient()
         {
-            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());            
-            IPAddress ipAddress = Dns.GetHostEntry("zrobi-VBuntu").AddressList[1];
-            IPEndPoint server = new IPEndPoint(ipAddress, 12345);            
-            client = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);            
-            client.Connect(server);
-            textBlock1.Text += "[+]Socket connected to " + client.RemoteEndPoint.ToString() + Environment.NewLine;
-            
-            
+            IPAddress ipAddress = IPAddress.Parse("192.168.56.101");
+            client = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);           
+            client.Connect("192.168.56.101", 12345);
+            textBlock1.Text += "[+]Socket kapcsolódott: " + client.RemoteEndPoint.ToString() + Environment.NewLine;            
+
             //RECEIVE
             int bytesRec = client.Receive(bytes);
             string receivedMessage = Encoding.ASCII.GetString(bytes, 0, bytesRec);
 
             if (receivedMessage.Equals("HelloP1"))
             {
-                textBlock1.Text += " [-]1.játékos vagyok! Várakozás a 2.játékosra!";
+                whoIam = 1;
+                isMyTurn = true;
+                textBlock1.Text += " [-]1.játékos vagyok! Várakozás a 2.játékosra!\n";                                
             }
             else if (receivedMessage.Equals("HelloP2"))
             {
-                textBlock1.Text += " [-]2.játékos vagyok!";
-            }            
+                whoIam = 2;
+
+                textBlock1.Text += " [-]2.játékos vagyok!\n";
+                textBlock1.Text += "[+]A játék elkezdődött!\n";
+
+                SendMessage("START");                
+            }
+            canIWait = true;
         }
 
         void SendMessage(string message)
@@ -69,13 +71,45 @@ namespace HeartsClient
             int bytesSent = client.Send(msg);
         }
 
+        void ReceiveMessage()
+        {
+            int bytesRec = client.Receive(bytes);
+            string receivedMessage = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+            CompareMessage(receivedMessage);
+        }
+                
+        void CompareMessage(string recMessage)
+        {
+            if (recMessage.Equals("cards1"))
+            {
+                textBlock1.Text += "  [-]Kártyáim:\n";
+            }
+            else if(recMessage.Equals("cards2"))
+            {
+                textBlock1.Text += "  [-]Kártyáim2:\n";
+            }
+        }
+
+        private async Task DelayedReceiveTask()
+        {
+            await Task.Delay(100);
+            if (!quitGame && canIWait)
+            {
+                ReceiveMessage();
+            }
+        }
+
         private void ConnectButton_Click(object sender, EventArgs e)
         {
             ConnectClient();
+
+            DelayedReceiveTask();
         }
 
         private void EndGameButton_Click(object sender, EventArgs e)
         {
+            SendMessage("VEGE");
+            quitGame = true;
             client.Shutdown(SocketShutdown.Both);
             client.Close();
             this.Close();
@@ -83,12 +117,12 @@ namespace HeartsClient
 
         private void GiveUpButton_Click(object sender, RoutedEventArgs e)
         {
-
+            SendMessage("FELADOM");
         }
 
         private void RetryButton_Click(object sender, RoutedEventArgs e)
         {
-
+            SendMessage("UJRA");
         }
     }
 }
--- heartsServer.c
+++ heartsServer.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
@@ -9,20 +10,214 @@
 #define PORT 12345
 #define BUFFERSIZE 4000
 
-//SERVER
+	char message[BUFFERSIZE+1];
+	char recMessage[BUFFERSIZE+1];
+	int bytes = BUFFERSIZE;
+	int rcvsize;
+
+	//Server
+	int serverSocket;
+	struct sockaddr_in serverAddr;
+			
+	//Clients
+	int clientSocket;
+	struct sockaddr_in clientAddr;	
+	int clientSocket2;
+	struct sockaddr_in clientAddr2;	
+
+	socklen_t addr_size;	
+	
+	bool quitGame = false;
+	bool isStarted = false;		
+	int whoseTurn = 2;
+	int firstPlayer = 1;	
+
+	int cards[52];
+	int p1Cards[13];
+	int p2Cards[13];
+	int p3Cards[13];
+	int p4Cards[13];
+
+void shufflingCards();
+void sendMessage(char msg[], int client);
+void receiveMessage(int client, int turn);
+void compareMessage(char * receivedMsg, int turn);
+void whoseFirst();
+bool include(int rCards[],int r);
+
+bool include(int rCards[],int r)
+{
+	for (int i = 0; i < 52; i++)
+	{
+		if (rCards[i] == r)
+		{
+			return true;
+		}						
+	}
+	return false;	
+}
+
+void shuffligCards()
+{		
+	for (int i = 0; i < 52; i++)
+	{
+		cards[i] = 0;
+	}	
+
+    for(int i = 0; i < 52; i++){
+		RAND:int r = (rand() % 52) + 1;
+
+	   if (!include(cards,r))
+	   {
+			cards[i] = (rand() % 52) + 1;
+	   }
+	   else
+	   {
+		   goto RAND;
+	   }	   	   	   	    
+   }
+
+   for(int i = 0; i < 13; i++)
+   {
+           p1Cards[i] = cards[i];
+           p2Cards[i] = cards[i+13];
+		   p3Cards[i] = cards[i+26];
+		   p4Cards[i] = cards[i+39];
+   }
+
+   whoseFirst();
+}
+
+void whoseFirst()
+{
+	for (int i = 0; i < 52; i++)
+	{
+		//TREFF 2
+		if (cards[i] == 41)
+		{
+			if (i < 13)
+			{
+				firstPlayer = 1;
+			}
+			else if(i > 12 && i < 26)
+			{
+				firstPlayer = 2;
+			}
+			else if(i > 25 && i < 39)
+			{
+				firstPlayer = 3;
+			}
+			else if(i > 38)
+			{
+				firstPlayer = 4;
+			}						
+		}		
+	}	
+}
+
+void sendMessage(char msg[], int client)
+{
+	strcpy(message, msg);
+	send(client, message, strlen(message), 0);
+}
+
+void receiveMessage(int client, int turn)
+{		
+ 	rcvsize = recv(client, recMessage, bytes, 0);
+	if (rcvsize >= 0)
+	{
+        compareMessage(recMessage, turn);
+    }
+	else
+	{		
+		printf("[!]Error in \"receiveMessage()\" from %d\n", &client);		
+	}
+}
+
+void compareMessage(char * receivedMsg, int turn)
+{
+	if (strcmp(receivedMsg, "START") == 0)
+    {
+		shuffligCards();
+
+		printf(" [-]A játék elkezdődött.\n");			
+				
+		printf("  [-]1. játékos kártyái: ");
+		for (int i = 0; i < 13; i++)
+		{
+			printf("%d ",p1Cards[i]);
+		}		
+		printf("\n");
+		printf("  [-]2. játékos kártyái: ");
+		for (int i = 0; i < 13; i++)
+		{
+			printf("%d ",p2Cards[i]);
+		}		
+		printf("\n");
+		printf("  [-]3. játékos kártyái: ");
+		for (int i = 0; i < 13; i++)
+		{
+			printf("%d ",p3Cards[i]);
+		}		
+		printf("\n");
+		printf("  [-]4. játékos kártyái: ");
+		for (int i = 0; i < 13; i++)
+		{
+			printf("%d ",p4Cards[i]);
+		}		
+		printf("\n");					
+		printf("   [-]A(z) %d. játékos kezd!\n", firstPlayer);
+
+		sendMessage("cards1",clientSocket);
+		sendMessage("cards2",clientSocket2);		
+	}
+	else if(strcmp(receivedMsg, "FELADOM") == 0)
+	{
+		if(turn == 1)
+		{
+			printf(" [-]Az első játékos feladta.\n");	
+		}
+		else
+		{
+			printf(" [-]A második játékos feladta.\n");	
+		}
+	}
+	else if(strcmp(receivedMsg, "UJRA") == 0)
+	{
+		if(turn == 1)
+		{
+			printf(" [-]Az első játékos újrakezdené.\n");	
+		}
+		else
+		{
+			printf(" [-]A második játékos újrakezdené.\n");	
+		}
+	}
+	else if(strcmp(receivedMsg, "VEGE") == 0)
+	{
+		if(turn == 1)
+		{
+			printf(" [-]Az első játékos kilépett.\n");			
+		}
+		else
+		{
+			printf(" [-]A második játékos kilépett.\n");	
+		}
+
+		quitGame = true;
+
+		printf(" [-]A játék véget ért.\n");
+		printf("[+]Closing the connection.\n");
+
+		close(clientSocket);
+		close(serverSocket);
+	}
+}
 
 void main(){
 
-	int serverSocket;
-	struct sockaddr_in serverAddr;
-
-	int clientSocket;
-	struct sockaddr_in clientAddr;	
-
-	socklen_t addr_size;
-	char message[BUFFERSIZE+1];
-	int bytes = BUFFERSIZE;
-	int rcvsize;
+	srand(time(NULL));
+	char on = 1;	
 
 	serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 	printf("[+]Server Socket Created Sucessfully.\n");
@@ -31,49 +226,39 @@ void main(){
 	//SERVER
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(PORT);	
-	serverAddr.sin_addr.s_addr = inet_addr("192.168.56.101");
-
-    //BIND
+	serverAddr.sin_addr.s_addr = inet_addr("192.168.56.101");	
+    
 	bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
 	printf("[+]Bind to Port number %d.\n", PORT);
-//P1
-    //LISTEN
+
+	//P1    
 	listen(serverSocket, 5);
-	printf("[+]Listening for P1\n");
-    
+	printf("[+]Listening for P1\n");    
 	clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddr, &addr_size);
-	printf("[+]Connected to P1\n");
+	printf("[+]Connected to P1\n");		
+	sendMessage("HelloP1", clientSocket);
 
-	//SEND
-	strcpy(message, "HelloP1");
-	send(clientSocket, message, strlen(message), 0);
-
-//P2
-	//LISTEN
+	//P2	
 	listen(serverSocket, 5);
-	printf("[+]Listening for P2\n");
-    
-	clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddr, &addr_size);
-	printf("[+]Connected to P2\n");
-
-	//SEND
-	strcpy(message, "HelloP2");
-	send(clientSocket, message, strlen(message), 0);
-
-/*	//RECEIVE
- 	rcvsize = recv(clientSocket, message, bytes, 0 );
-	if (rcvsize < 0)
-	{
-        printf("Error\n");
-    }
-	else
-	{		
-		printf(" [-]Message received: %s\n", message);	
-	}*/
+	printf("[+]Listening for P2\n");    
+	clientSocket2 = accept(serverSocket, (struct sockaddr *) &clientAddr2, &addr_size);
+	printf("[+]Connected to P2\n");	
+	sendMessage("HelloP2", clientSocket2);
 	
-    
+	while(!quitGame)
+	{
+		if (whoseTurn == 1)
+		{
+			receiveMessage(clientSocket, whoseTurn);
+		}
+		else if (whoseTurn == 2)
+		{
+			receiveMessage(clientSocket2, whoseTurn);
+		}					
+	}
 
-	printf("[+]Closing the connection.\n");
-	close(clientSocket);
-	close(serverSocket);
+    printf("[+]Closing the connection.\n");
+
+		close(clientSocket);
+		close(serverSocket);
 }
