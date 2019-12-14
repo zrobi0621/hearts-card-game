23ae5cf28f4fcb8fc41aba8e523a00c146cd90b9
zrobi0621 <33635244+zrobi0621@users.noreply.github.com>
2019-12-14 17:14:14 +0100

    V2

--- HeartsClient/HeartsClient/MainWindow.xaml.cs
+++ HeartsClient/HeartsClient/MainWindow.xaml.cs
@@ -30,10 +30,18 @@ namespace HeartsClient
         bool isMyTurn = false;
         bool quitGame = false;
         bool canIWait = false;
+        List<int> myCards = new List<int>();
+        Image[] myCardsImages;
+
+        int p1PickedCard = 0;
+        int p2PickedCard = 0;
+        int p3PickedCard = 0;
+        int p4PickedCard = 0;
 
         public MainWindow()
         {
-            InitializeComponent();            
+            InitializeComponent();
+            myCardsImages = new Image[]{Card0, Card1, Card2, Card3, Card4, Card5, Card6, Card7, Card8, Card9, Card10, Card11, Card12};
         }
 
         public void ConnectClient()
@@ -50,16 +58,25 @@ namespace HeartsClient
             if (receivedMessage.Equals("HelloP1"))
             {
                 whoIam = 1;
-                isMyTurn = true;
+                //isMyTurn = true;
+                WhoIAmLabel.Content = "1.Játékos";
+                TLLabel.Content = "2.Játékos \\/";
+                TRLabel.Content = "(= 3.Játékos";
+                BRLabel.Content = "4.játékos /\\";
+
                 textBlock1.Text += " [-]1.játékos vagyok! Várakozás a 2.játékosra!\n";                                
             }
             else if (receivedMessage.Equals("HelloP2"))
             {
                 whoIam = 2;
+                WhoIAmLabel.Content = "2.Játékos";
+                TLLabel.Content = "3.Játékos \\/";
+                TRLabel.Content = "(= 4.Játékos";
+                BRLabel.Content = "1.játékos /\\";
 
                 textBlock1.Text += " [-]2.játékos vagyok!\n";
                 textBlock1.Text += "[+]A játék elkezdődött!\n";
-
+                
                 SendMessage("START");                
             }
             canIWait = true;
@@ -67,7 +84,9 @@ namespace HeartsClient
 
         void SendMessage(string message)
         {
-            byte[] msg = Encoding.UTF8.GetBytes(message);
+            
+            textBlock1.Text += message + "\n";
+            byte[] msg = Encoding.UTF8.GetBytes(message);                        
             int bytesSent = client.Send(msg);
         }
 
@@ -80,13 +99,84 @@ namespace HeartsClient
                 
         void CompareMessage(string recMessage)
         {
-            if (recMessage.Equals("cards1"))
+            string[] token = recMessage.Split('.');
+            if (token[0].Equals("C1"))
             {
-                textBlock1.Text += "  [-]Kártyáim:\n";
+                LoadMyCards(token);
             }
-            else if(recMessage.Equals("cards2"))
+            else if(token[0].Equals("C2"))
+            {                
+                LoadMyCards(token);
+            }
+            else if(token[0].Equals("T1"))
+            {                
+                p2PickedCard = int.Parse(token[1]);
+                p3PickedCard = int.Parse(token[2]);
+                p4PickedCard = int.Parse(token[3]);
+                textBlock1.Text += "T1: " + token[1] + " " + token[2] + " " + token[3];
+                PickedCards();
+            }
+            else if (token[0].Equals("T2"))
             {
-                textBlock1.Text += "  [-]Kártyáim2:\n";
+                p1PickedCard = int.Parse(token[1]);
+                p3PickedCard = int.Parse(token[2]);
+                p4PickedCard = int.Parse(token[3]);
+                textBlock1.Text += "T2: " + token[1] + " " + token[2] + " " + token[3];
+                PickedCards();
+            }
+        }
+
+        private void PickedCards()
+        {
+            if (whoIam == 1)
+            {                
+                CardLeft.Source = new BitmapImage(new Uri(@"/Resources/Cards/(" + p2PickedCard.ToString() + ").jpg", UriKind.Relative));
+                CardTop.Source = new BitmapImage(new Uri(@"/Resources/Cards/(" + p3PickedCard.ToString() + ").jpg", UriKind.Relative));
+                CardRight.Source = new BitmapImage(new Uri(@"/Resources/Cards/(" + p4PickedCard.ToString() + ").jpg", UriKind.Relative));
+            }
+            else if(whoIam == 2)
+            {
+                CardLeft.Source = new BitmapImage(new Uri(@"/Resources/Cards/(" + p3PickedCard.ToString() + ").jpg", UriKind.Relative));
+                CardTop.Source = new BitmapImage(new Uri(@"/Resources/Cards/(" + p4PickedCard.ToString() + ").jpg", UriKind.Relative));
+                CardRight.Source = new BitmapImage(new Uri(@"/Resources/Cards/(" + p1PickedCard.ToString() + ").jpg", UriKind.Relative));
+            }
+        }
+
+        private void LoadMyCards(string[] token)
+        {
+            textBlock1.Text += "  [-]Kártyáim: ";
+
+            for (int i = 1; i < 14; i++)
+            {
+                myCards.Add(int.Parse(token[i]));
+                textBlock1.Text += int.Parse(token[i]) + " ";
+            }        
+
+            textBlock1.Text += "\n";
+
+            for (int i = 0; i < myCardsImages.Length; i++)
+            {
+                myCardsImages[i].Source = new BitmapImage(new Uri(@"/Resources/Cards/(" + myCards[i].ToString() + ").jpg", UriKind.Relative));
+            }
+
+            if (!CanIStart())
+            {
+                DelayedReceiveTask();
+            }
+        }
+
+        private bool CanIStart()
+        {
+            //TREFF 2
+            if (myCards.Contains(41))
+            {
+                textBlock1.Text += "   [-]Én kezdek, nálam a Treff 2!\n";
+                return true;
+            }
+            else
+            {
+                textBlock1.Text += "   [-]Nem én kezdek!\n";
+                return false;
             }
         }
 
@@ -123,6 +213,21 @@ namespace HeartsClient
         private void RetryButton_Click(object sender, RoutedEventArgs e)
         {
             SendMessage("UJRA");
+        }
+
+        private void CardImage_Click(object sender, MouseButtonEventArgs e)
+        {
+            Image image = sender as Image;
+            string name = image.Name;
+            name = name.Substring(4);
+            if (whoIam == 1)
+            {
+                SendMessage("P1P" + "." + myCards[int.Parse(name)]);
+            }
+            else if(whoIam == 2)
+            {
+                SendMessage("P2P" + "." + myCards[int.Parse(name)]);
+            }            
         }
     }
 }
--- heartsServer.c
+++ heartsServer.c
@@ -38,12 +38,20 @@
 	int p3Cards[13];
 	int p4Cards[13];
 
+	int p1PickedCard = 0;
+	int p2PickedCard = 0;
+	int p3PickedCard = 0;
+	int p4PickedCard = 0;
+
+bool include(int rCards[],int r);
 void shufflingCards();
+void whoseFirst();
 void sendMessage(char msg[], int client);
 void receiveMessage(int client, int turn);
 void compareMessage(char * receivedMsg, int turn);
-void whoseFirst();
-bool include(int rCards[],int r);
+void buildCardsMessage(int player);
+void buildTurnMessage(int player,int p1Pick, int p2Pick, int p3Pick, int p4Pick);
+void pickCard(int player);
 
 bool include(int rCards[],int r)
 {
@@ -65,11 +73,12 @@ void shuffligCards()
 	}	
 
     for(int i = 0; i < 52; i++){
-		RAND:int r = (rand() % 52) + 1;
+		RAND:;
+		int r = (rand() % 52) + 1;
 
 	   if (!include(cards,r))
 	   {
-			cards[i] = (rand() % 52) + 1;
+			cards[i] = r;
 	   }
 	   else
 	   {
@@ -113,6 +122,29 @@ void whoseFirst()
 			}						
 		}		
 	}	
+	whoseTurn = firstPlayer;
+}
+
+void pickCard(int player)
+{
+	int r = rand() % 13;
+
+	if (player == 1)
+	{
+		p1PickedCard = p1Cards[r];	
+	}
+	else if(player == 2)
+	{
+		p2PickedCard = p2Cards[r];	
+	}
+	else if(player == 3)
+	{
+		p3PickedCard = p3Cards[r];	
+	}
+	else if(player == 4)
+	{
+		p4PickedCard = p4Cards[r];	
+	}	
 }
 
 void sendMessage(char msg[], int client)
@@ -122,8 +154,10 @@ void sendMessage(char msg[], int client)
 }
 
 void receiveMessage(int client, int turn)
-{		
+{			
+	strcpy(recMessage, "");
  	rcvsize = recv(client, recMessage, bytes, 0);
+	 
 	if (rcvsize >= 0)
 	{
         compareMessage(recMessage, turn);
@@ -134,21 +168,51 @@ void receiveMessage(int client, int turn)
 	}
 }
 
+void buildCardsMessage(int player)
+{
+	int* msgCards;
+	if(player == 1)
+	{
+		msgCards = p1Cards;
+		sprintf(message,"%s.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d","C1",msgCards[0],msgCards[1],msgCards[2],msgCards[3],msgCards[4],msgCards[5],msgCards[6],msgCards[7],msgCards[8],msgCards[9],msgCards[10],msgCards[11],msgCards[12]);
+	}
+	else if(player == 2)
+	{
+		msgCards = p2Cards;
+		sprintf(message,"%s.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d","C2",msgCards[0],msgCards[1],msgCards[2],msgCards[3],msgCards[4],msgCards[5],msgCards[6],msgCards[7],msgCards[8],msgCards[9],msgCards[10],msgCards[11],msgCards[12]);
+	}	
+}
+
+void buildTurnMessage(int player, int p1Pick, int p2Pick, int p3Pick, int p4Pick)
+{	
+	if(player == 1)
+	{
+		sprintf(message,"%s.%d.%d.%d","T1",p2Pick,p3Pick,p4Pick);
+		printf("       %s.%d.%d.%d","T1",p2Pick,p3Pick,p4Pick);
+	}
+	else if(player == 2)
+	{
+		sprintf(message,"%s.%d.%d.%d","T2",p1Pick,p3Pick,p4Pick);
+		printf("       %s.%d.%d.%d","T2",p1Pick,p3Pick,p4Pick);
+	}	
+}
+
 void compareMessage(char * receivedMsg, int turn)
 {
 	if (strcmp(receivedMsg, "START") == 0)
     {
-		shuffligCards();
+		memset(recMessage, 0, BUFFERSIZE+1);
+		shuffligCards();		
 
 		printf(" [-]A játék elkezdődött.\n");			
 				
-		printf("  [-]1. játékos kártyái: ");
+		printf("  [-]1. játékos(P1) kártyái: ");
 		for (int i = 0; i < 13; i++)
 		{
 			printf("%d ",p1Cards[i]);
 		}		
 		printf("\n");
-		printf("  [-]2. játékos kártyái: ");
+		printf("  [-]2. játékos(P2) kártyái: ");
 		for (int i = 0; i < 13; i++)
 		{
 			printf("%d ",p2Cards[i]);
@@ -168,8 +232,28 @@ void compareMessage(char * receivedMsg, int turn)
 		printf("\n");					
 		printf("   [-]A(z) %d. játékos kezd!\n", firstPlayer);
 
-		sendMessage("cards1",clientSocket);
-		sendMessage("cards2",clientSocket2);		
+		buildCardsMessage(1);
+		sendMessage(message,clientSocket);
+		buildCardsMessage(2);
+		sendMessage(message,clientSocket2);		
+
+		if (whoseTurn == 3 || whoseTurn == 4)
+		{		
+			pickCard(3);
+			pickCard(4);
+			whoseTurn = 4;
+		}
+
+	/*else if(whoseTurn == 1)
+		{
+			//waitForP1Pick
+			//SendP2 P1Pick
+		}
+		else if(whoseTurn == 2)
+		{
+			//waitForP2Pick
+			//SendP1 P2Pick,P3Pick,P4Pick
+		}*/		
 	}
 	else if(strcmp(receivedMsg, "FELADOM") == 0)
 	{
@@ -195,7 +279,7 @@ void compareMessage(char * receivedMsg, int turn)
 	}
 	else if(strcmp(receivedMsg, "VEGE") == 0)
 	{
-		if(turn == 1)
+		if(whoseTurn == 1)
 		{
 			printf(" [-]Az első játékos kilépett.\n");			
 		}
@@ -212,6 +296,24 @@ void compareMessage(char * receivedMsg, int turn)
 		close(clientSocket);
 		close(serverSocket);
 	}
+	else
+	{
+		char *token;
+		token = strtok(receivedMsg,".");
+		
+		if (strcmp(token,"P1P") == 0)
+		{
+			token = strtok(NULL,".");
+			printf("P1P: %s",token);
+			whoseTurn = 2;
+		}
+		else if(strcmp(token,"P2P") == 0)
+		{
+			token = strtok(NULL,".");
+			printf("P2P: %s",token);
+			whoseTurn = 3;
+		}								
+	}	
 }
 
 void main(){
@@ -249,16 +351,27 @@ void main(){
 	{
 		if (whoseTurn == 1)
 		{
+			printf("[ReceiveP1]\n");
+			printf("%s",recMessage);
 			receiveMessage(clientSocket, whoseTurn);
 		}
 		else if (whoseTurn == 2)
 		{
+			printf("[ReceiveP2]\n");
+			printf("%s",recMessage);
 			receiveMessage(clientSocket2, whoseTurn);
 		}					
+		else if (whoseTurn == 4)
+		{
+			printf("[SendP1]\n");
+			buildTurnMessage(1,p1PickedCard,p2PickedCard,p3PickedCard,p4PickedCard);
+			sendMessage(message,clientSocket);
+			whoseTurn = 1;
+		}
 	}
 
     printf("[+]Closing the connection.\n");
 
-		close(clientSocket);
-		close(serverSocket);
+	close(clientSocket);
+	close(serverSocket);
 }
