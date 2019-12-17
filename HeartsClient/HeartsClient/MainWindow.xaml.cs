e8b80608a8c9b5e612a01a8dfb60eca85ae01179
zrobi0621 <33635244+zrobi0621@users.noreply.github.com>
2019-12-17 04:31:54 +0100

    .

--- HeartsClient/HeartsClient/MainWindow.xaml.cs
+++ HeartsClient/HeartsClient/MainWindow.xaml.cs
@@ -23,13 +23,14 @@ namespace HeartsClient
     /// </summary>
     public partial class MainWindow : Window
     {
-        public Socket client;        
+        public Socket client;                
         public byte[] bytes = new byte[1024];
 
-        int whoIam = 0;
-        bool isMyTurn = false;
+        int whoIam = 0;        
         bool quitGame = false;
         bool canIWait = false;
+        bool isStarted = false;
+
         List<int> myCards = new List<int>();
         Image[] myCardsImages;
 
@@ -37,6 +38,9 @@ namespace HeartsClient
         int p2PickedCard = 0;
         int p3PickedCard = 0;
         int p4PickedCard = 0;
+
+        int turn = 0;
+        string[] tokenTmp;
 
         public MainWindow()
         {
@@ -76,7 +80,7 @@ namespace HeartsClient
 
                 textBlock1.Text += " [-]2.játékos vagyok!\n";
                 textBlock1.Text += "[+]A játék elkezdődött!\n";
-                
+
                 SendMessage("START");                
             }
             canIWait = true;
@@ -100,11 +104,15 @@ namespace HeartsClient
         {
             string[] token = recMessage.Split('.');
             if (token[0].Equals("C1"))
-            {
+            {                
+                isStarted = true;
+                turn = 0;
                 LoadMyCards(token);
             }
             else if(token[0].Equals("C2"))
             {                
+                isStarted = true;
+                turn = 0;
                 LoadMyCards(token);
             }
             else if(token[0].Equals("T1"))
@@ -114,6 +122,7 @@ namespace HeartsClient
                 p4PickedCard = int.Parse(token[3]);
                 textBlock1.Text += "T1: " + token[1] + " " + token[2] + " " + token[3]+"\n";
                 PickedCards();
+                tokenTmp = token;
             }
             else if (token[0].Equals("T2"))
             {
@@ -122,6 +131,29 @@ namespace HeartsClient
                 p4PickedCard = int.Parse(token[3]);
                 textBlock1.Text += "T2: " + token[1] + " " + token[2] + " " + token[3]+"\n";
                 PickedCards();
+                tokenTmp = token;
+            }
+            else if(token[0].Equals("WIN"))
+            {
+                if (MessageBox.Show(token[1] + ".játékos nyert! Újra?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                {
+                    if (whoIam == 1)
+                    {
+                        ResetGame();
+                    }
+                    else if (whoIam == 2)
+                    {
+                        SendMessage("START");
+                    }
+                }
+                else
+                {
+                    SendMessage("VEGE");
+                    quitGame = true;
+                    client.Shutdown(SocketShutdown.Both);
+                    client.Close();
+                    this.Close();
+                }
             }
             else if(token[0].Equals("VEGE"))
             {
@@ -129,11 +161,13 @@ namespace HeartsClient
                 {
                     textBlock1.Text += "A 2.játékos kilépett, a játéknak VÉGE!";
                     MessageBox.Show("A 2.játékos kilépett, a játéknak VÉGE!");
+                    this.Close();
                 }
                 else if(whoIam == 2)
                 {
                     textBlock1.Text += "Az 1.játékos kilépett, a játéknak VÉGE!";
                     MessageBox.Show("Az 1.játékos kilépett, a játéknak VÉGE!");
+                    this.Close();
                 }
             }
             else if (token[0].Equals("FELADOM"))
@@ -141,7 +175,25 @@ namespace HeartsClient
                 if (whoIam == 1)
                 {
                     textBlock1.Text += "A 2.játékos feladta!";
-                    MessageBox.Show("A 2.játékos feladta!");
+                    if (MessageBox.Show("Az 2.játékos feladta! Új játék?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                    {
+                        if (whoIam == 1)
+                        {
+                            ResetGame();
+                        }
+                        else if (whoIam == 2)
+                        {
+                            SendMessage("START");
+                        }                        
+                    }
+                    else
+                    {
+                        SendMessage("VEGE");
+                        quitGame = true;
+                        client.Shutdown(SocketShutdown.Both);
+                        client.Close();
+                        this.Close();
+                    }
                 }
                 else if (whoIam == 2)
                 {
@@ -154,14 +206,62 @@ namespace HeartsClient
                 if (whoIam == 1)
                 {
                     textBlock1.Text += "A 2.játékos újrakezedené!";
-                    MessageBox.Show("A 2.játékos újrakezdené!");
+                    if (MessageBox.Show("A 2.játékos újrakezdené! Új játék?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                    {                        
+                        ResetGame();                        
+                    }
+                    else
+                    {
+                        SendMessage("VEGE");
+                        quitGame = true;
+                        client.Shutdown(SocketShutdown.Both);
+                        client.Close();
+                        this.Close();
+                    }
                 }
                 else if (whoIam == 2)
                 {
                     textBlock1.Text += "Az 1.játékos újrakezdené!";
-                    MessageBox.Show("Az 1.játékos újrakezdené!");
+                    if (MessageBox.Show("Az 1.játékos újrakezdené! Új játék?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                    {
+                        ResetTurn();
+                        SendMessage("START");
+                        DelayedReceiveTask();
+                    }
+                    else
+                    {
+                        SendMessage("VEGE");
+                        quitGame = true;
+                        client.Shutdown(SocketShutdown.Both);
+                        client.Close();
+                        this.Close();
+                    }
                 }
             }
+        }
+
+        private void ResetTurn()
+        {
+            myCards.Clear();
+            p1PickedCard = 0;
+            p2PickedCard = 0;
+            p3PickedCard = 0;
+            p4PickedCard = 0;
+
+            CardTop.Source = null;
+            CardLeft.Source = null;
+            CardRight.Source = null;
+            CardBottom.Source = null;
+
+            isStarted = false;
+
+            textBlock1.Text = "";
+        }
+
+        private void ResetGame()
+        {
+            ResetTurn();            
+            DelayedReceiveTask();
         }
 
         private void PickedCards()
@@ -196,9 +296,9 @@ namespace HeartsClient
             {
                 myCardsImages[i].Source = new BitmapImage(new Uri(@"/Resources/Cards/(" + myCards[i].ToString() + ").jpg", UriKind.Relative));
             }
-
+            
             if (!CanIStart())
-            {
+            {                
                 DelayedReceiveTask();
             }
         }
@@ -246,6 +346,18 @@ namespace HeartsClient
         private void GiveUpButton_Click(object sender, RoutedEventArgs e)
         {
             SendMessage("FELADOM");
+            if (MessageBox.Show("Feladtad! Új játék?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                SendMessage("UJRA");
+            }
+            else
+            {
+                SendMessage("VEGE");
+                quitGame = true;
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+                this.Close();
+            }
         }
 
         private void RetryButton_Click(object sender, RoutedEventArgs e)
@@ -272,7 +384,28 @@ namespace HeartsClient
             image.Source = null;
             CardBottom.Source = new BitmapImage(new Uri(@"/Resources/Cards/(" + myCards[pickedImage] + ").jpg", UriKind.Relative));
 
-            DelayedReceiveTask();
+            turn++;
+
+            if (turn == 13 && whoIam == 2)
+            {
+                DelayedReceiveTask();
+                
+                ResetTurn();
+                
+                SendMessage("START");                                                                    
+            }
+            else if(turn == 13 && whoIam == 1)
+            {                                
+                DelayedReceiveTask();
+
+                ResetTurn();
+
+                DelayedReceiveTask();                
+            }
+            else
+            {
+                DelayedReceiveTask();
+            }            
         }
     }
 }
--- heartsServer.c
+++ heartsServer.c
@@ -10,6 +10,8 @@
 #define PORT 12345
 #define BUFFERSIZE 4000
 
+#define MAXPOINT 20
+
 	char message[BUFFERSIZE+1];
 	char recMessage[BUFFERSIZE+1];
 	int bytes = BUFFERSIZE;
@@ -30,8 +32,13 @@
 	bool quitGame = false;
 	bool isStarted = false;		
 	bool isP1First = false;
+	bool waiting = false;
+
 	int whoseTurn = 2;
 	int firstPlayer = 1;	
+	int turnCount = 1;
+	int bigTurnCount = 0;
+	int winner = 0;
 
 	int cards[52];
 	int p1Cards[13];
@@ -43,6 +50,20 @@
 	int p2PickedCard = 0;
 	int p3PickedCard = 0;
 	int p4PickedCard = 0;
+	int pickCount = 0;
+
+	//STRENGTHS
+	int s1 = 0;
+	int s2 = 0;
+	int s3 = 0;
+	int s4 = 0;
+
+	//POINTS
+	int p1Points = 0;
+	int p2Points = 0;
+	int p3Points = 0;
+	int p4Points = 0;
+	int tPoint = 0;
 
 bool include(int rCards[],int r);
 void shufflingCards();
@@ -51,11 +72,16 @@ void sendMessage(char msg[], int client);
 void receiveMessage(int client, int turn);
 void compareMessage(char * receivedMsg, int turn);
 void buildCardsMessage(int player);
-void buildTurnMessage(int player,int p1Pick, int p2Pick, int p3Pick, int p4Pick);
+void buildTurnMessage(int player,int p1Pick, int p2Pick, int p3Pick, int p4Pick, int p1p, int p2p, int p3p, int p4p);
 void pickCard(int player);
+void calculatePoints();
+int bestStrength();
+void resetTurn();
+void resetGame();
+void getWinner();
 
 bool include(int rCards[],int r)
-{
+{	
 	for (int i = 0; i < 52; i++)
 	{
 		if (rCards[i] == r)
@@ -123,7 +149,43 @@ void whoseFirst()
 			}						
 		}		
 	}	
-	whoseTurn = firstPlayer;
+	//whoseTurn = firstPlayer;
+
+	if (firstPlayer == 2)
+	{
+		for (int i = 0; i < 13; i++)
+		{
+			if(p2Cards[i] == 41)
+			{
+				p2Cards[i] = p1Cards[i];
+				p1Cards[i] = 41;
+			}
+		}		
+	}
+	if (firstPlayer == 3)
+	{
+		for (int i = 0; i < 13; i++)
+		{
+			if(p3Cards[i] == 41)
+			{
+				p3Cards[i] = p1Cards[i];
+				p1Cards[i] = 41;
+			}
+		}		
+	}
+	if (firstPlayer == 4)
+	{
+		for (int i = 0; i < 13; i++)
+		{
+			if(p4Cards[i] == 41)
+			{
+				p4Cards[i] = p1Cards[i];
+				p1Cards[i] = 41;
+			}
+		}		
+	}
+	firstPlayer = 1;
+	whoseTurn = 1;
 }
 
 void pickCard(int player)
@@ -144,7 +206,7 @@ void pickCard(int player)
 	}
 	else if(player == 4)
 	{
-		p4PickedCard = p4Cards[r];	
+		p4PickedCard = p4Cards[r];					
 	}	
 }
 
@@ -177,32 +239,34 @@ void buildCardsMessage(int player)
 	{
 		msgCards = p1Cards;
 		sprintf(message,"%s.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d","C1",msgCards[0],msgCards[1],msgCards[2],msgCards[3],msgCards[4],msgCards[5],msgCards[6],msgCards[7],msgCards[8],msgCards[9],msgCards[10],msgCards[11],msgCards[12]);
+		printf("%s.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d","C1",msgCards[0],msgCards[1],msgCards[2],msgCards[3],msgCards[4],msgCards[5],msgCards[6],msgCards[7],msgCards[8],msgCards[9],msgCards[10],msgCards[11],msgCards[12]);
 	}
 	else if(player == 2)
 	{
 		msgCards = p2Cards;
 		sprintf(message,"%s.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d","C2",msgCards[0],msgCards[1],msgCards[2],msgCards[3],msgCards[4],msgCards[5],msgCards[6],msgCards[7],msgCards[8],msgCards[9],msgCards[10],msgCards[11],msgCards[12]);
+		printf("%s.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d.%d","C2",msgCards[0],msgCards[1],msgCards[2],msgCards[3],msgCards[4],msgCards[5],msgCards[6],msgCards[7],msgCards[8],msgCards[9],msgCards[10],msgCards[11],msgCards[12]);
 	}	
 }
 
-void buildTurnMessage(int player, int p1Pick, int p2Pick, int p3Pick, int p4Pick)
+void buildTurnMessage(int player, int p1Pick, int p2Pick, int p3Pick, int p4Pick, int p1p, int p2p, int p3p, int p4p)
 {	
 	if(player == 1)
 	{
-		sprintf(message,"%s.%d.%d.%d","T1",p2Pick,p3Pick,p4Pick);
+		sprintf(message,"%s.%d.%d.%d.%d.%d.%d.%d","T1",p2Pick,p3Pick,p4Pick,p1p,p2p,p3p,p4p);
 		printf("       %s.%d.%d.%d\n","T1",p2Pick,p3Pick,p4Pick);
 	}
 	else if(player == 2)
 	{
-		sprintf(message,"%s.%d.%d.%d","T2",p1Pick,p3Pick,p4Pick);
+		sprintf(message,"%s.%d.%d.%d.%d.%d.%d.%d","T2",p1Pick,p3Pick,p4Pick,p1p,p2p,p3p,p4p);
 		printf("       %s.%d.%d.%d\n","T2",p1Pick,p3Pick,p4Pick);
 	}	
 }
 
 void compareMessage(char * receivedMsg, int turn)
 {
-	if (strcmp(receivedMsg, "START") == 0)
-    {
+	if (strncmp(receivedMsg, "START",5) == 0 && !isStarted)
+    {			
 		memset(recMessage, 0, BUFFERSIZE+1);
 		shuffligCards();		
 
@@ -235,35 +299,51 @@ void compareMessage(char * receivedMsg, int turn)
 		printf("   [-]A(z) %d. játékos kezd!\n", firstPlayer);
 
 		buildCardsMessage(1);
-		sendMessage(message,clientSocket);
+		printf("küldés1nek\n");
+		sendMessage(message,clientSocket);				
 		buildCardsMessage(2);
-		sendMessage(message,clientSocket2);
+		printf("küldés2nek\n");
+		sendMessage(message,clientSocket2);		
+
+		isStarted = true;		
+		
+	}	
+	else if(strncmp(receivedMsg, "START",5) == 0 )
+	{
+		buildCardsMessage(2);
+		printf("__küldés2nek\n");
+		sendMessage(message,clientSocket2);		
+		buildCardsMessage(1);
+		printf("__küldés1nek\n");
+		sendMessage(message,clientSocket);		
 	}
 	else if(strcmp(receivedMsg, "FELADOM") == 0)
 	{
-		if(turn == 1)
+		if(whoseTurn == 1)
 		{
 			printf(" [-]Az első játékos feladta.\n");
-			sendMessage("FELADOM",clientSocket2);		
+			sendMessage("UJRA",clientSocket2);		
 		}
 		else
 		{
 			printf(" [-]A második játékos feladta.\n");	
-			sendMessage("FELADOM",clientSocket);	
+			sendMessage("UJRA",clientSocket);	
 		}
+		resetGame();		
 	}
 	else if(strcmp(receivedMsg, "UJRA") == 0)
 	{
-		if(turn == 1)
+		if(whoseTurn == 1)
 		{
 			printf(" [-]Az első játékos újrakezdené.\n");	
-			sendMessage("UJRA",clientSocket2);	
+			sendMessage("UJRA",clientSocket2);				
 		}
 		else
 		{
 			printf(" [-]A második játékos újrakezdené.\n");	
-			sendMessage("UJRA",clientSocket);	
+			sendMessage("UJRA",clientSocket);				
 		}
+		waiting = false;			
 	}
 	else if(strcmp(receivedMsg, "VEGE") == 0)
 	{
@@ -296,9 +376,14 @@ void compareMessage(char * receivedMsg, int turn)
 			token = strtok(NULL,".");
 			printf("P1P:%s\n",token);
 			p1PickedCard = atoi(token);
-			buildTurnMessage(2,p1PickedCard,p2PickedCard,p3PickedCard,p4PickedCard);
+			buildTurnMessage(2,p1PickedCard,p2PickedCard,p3PickedCard,p4PickedCard,p1Points,p2Points,p3Points,p4Points);
 			sendMessage(message,clientSocket2);
 			whoseTurn = 2;
+
+			if (firstPlayer != 2)
+			{
+				pickCount++;
+			}			
 		}
 		else if(strcmp(token,"P2P") == 0)
 		{
@@ -306,8 +391,229 @@ void compareMessage(char * receivedMsg, int turn)
 			printf("P2P:%s\n",token);
 			p2PickedCard = atoi(token);
 			whoseTurn = 3;
+
+			if (firstPlayer == 2)
+			{
+				pickCount++;
+			}
+			turnCount++;
 		}								
+	}		
+}
+
+void calculatePoints()
+{	
+	tPoint = 0;
+
+	//P1
+	if (p1PickedCard == 1 || p1PickedCard == 14 || p1PickedCard == 27 || p1PickedCard == 40)
+		{ s1 = 13;}
+	else{
+		if (p1PickedCard > 14 && p1PickedCard < 27)
+			{ s1 = p1PickedCard - 13;
+				tPoint++;}
+		else if(p1PickedCard > 26 && p1PickedCard < 40){
+				s1 = p1PickedCard - 26;
+				if (p1PickedCard == 38)
+					{ tPoint += 13;}
+			}
+		else if(p1PickedCard > 39)
+			{ s1 = p1PickedCard - 39;}		
+	}
+
+	//P2
+	if (p2PickedCard == 1 || p2PickedCard == 14 || p2PickedCard == 27 || p2PickedCard == 40)
+		{ s2 = 13;}
+	else{
+		if (p2PickedCard > 14 && p2PickedCard < 27)
+			{ s2 = p2PickedCard - 13;
+				tPoint++;}
+		else if(p2PickedCard > 26 && p2PickedCard < 40){ 
+				s2 = p2PickedCard - 26;
+				if (p2PickedCard == 38)
+					{ tPoint += 13;}
+			}
+		else if(p2PickedCard > 39)
+			{ s2 = p2PickedCard - 39;}		
+	}
+
+	//P3
+	if (p3PickedCard == 1 || p3PickedCard == 14 || p3PickedCard == 27 || p3PickedCard == 40)
+		{ s3 = 13;}
+	else{
+		if (p3PickedCard > 14 && p3PickedCard < 27)
+			{ s3 = p3PickedCard - 13;
+				tPoint++;}
+		else if(p3PickedCard > 26 && p3PickedCard < 40){ 
+				s3 = p3PickedCard - 26;
+				if (p3PickedCard == 38)
+					{ tPoint += 13;}
+			}
+		else if(p3PickedCard > 39)
+			{ s3 = p3PickedCard - 39;}		
+	}
+
+	//P4
+	if (p4PickedCard == 1 || p4PickedCard == 14 || p4PickedCard == 27 || p4PickedCard == 40)
+		{ s4 = 13;}
+	else{
+		if (p4PickedCard > 14 && p4PickedCard < 27)
+			{ s4 = p4PickedCard - 13;
+				tPoint++;}
+		else if(p4PickedCard > 26 && p4PickedCard < 40){
+				s4 = p4PickedCard - 26;
+				if (p4PickedCard == 38)
+					{ tPoint += 13;}
+			}
+		else if(p1PickedCard > 39)
+			{ s4 = p4PickedCard - 39;}		
+	}		
+}
+
+int bestStrength()
+{
+	int max = 0;
+
+	//S1
+	if (s1 >= max)	{
+		max = s1;
+	}
+	//S2
+	if (s2 >= max)	{
+		max = s2;
+	}
+	//S3
+	if (s3 >= max)	{
+		max = s3;
+	}
+	//S4
+	if (s4 >= max)	{
+		max = s4;
+	}
+
+	if (max == s1){		
+		p1Points += tPoint;
+		return 1;
+	}
+	else if(max == s2){
+		p2Points += tPoint;
+		return 2;
+	}
+	else if(max == s3){
+		p3Points += tPoint;
+		return 3;
+	}
+	else if(max == s4){
+		p4Points += tPoint;
+		return 4;
 	}	
+}
+
+void resetTurn()
+{		
+	if (winner != 0)
+	{
+		printf("****NYERTES: %d.játékos****",winner);
+
+		if (winner == 1)
+		{
+			sendMessage("WIN.1",clientSocket);
+			sendMessage("WIN.1",clientSocket2);
+		}
+		else if( winner == 2)
+		{
+			sendMessage("WIN.2",clientSocket);
+			sendMessage("WIN.2",clientSocket2);
+		}
+		else if( winner == 3)
+		{
+			sendMessage("WIN.3",clientSocket);
+			sendMessage("WIN.3",clientSocket2);
+		}
+		else if( winner == 4)
+		{
+			sendMessage("WIN.4",clientSocket);
+			sendMessage("WIN.4",clientSocket2);
+		}		
+	}
+	else
+	{
+		if (bigTurnCount >= 2)
+		{
+			buildTurnMessage(1,p1PickedCard,p2PickedCard,p3PickedCard,p4PickedCard,p1Points,p2Points,p3Points,p4Points);
+			sendMessage(message,clientSocket);
+		}
+		
+		turnCount = 1;
+		pickCount = 0;
+		isStarted = false;
+		p1PickedCard = 0;
+		p2PickedCard = 0;		
+	}			
+}
+
+void resetGame()
+{	
+	turnCount = 1;
+	pickCount = 0;
+	isStarted = false;
+	p1PickedCard = 0;
+	p2PickedCard = 0;
+	p3PickedCard = 0;
+	p4PickedCard = 0;
+	winner = 0;
+}
+
+void getWinner()
+{
+	int max = 0;
+	int min = 500;
+
+	if (p1Points >= MAXPOINT || p2Points >= MAXPOINT || p3Points >= MAXPOINT || p4Points >= MAXPOINT)
+	{				
+		if (p1Points >= max)	{
+			max = p1Points;
+		}
+		if (p2Points >= max)	{
+			max = p2Points;
+		}
+		if (p3Points >= max)	{
+			max = p3Points;
+		}
+		if (p4Points >= max)	{
+			max = p4Points;
+		}
+
+		if (p1Points <= min)	{
+			min = p1Points;
+		}
+		if (p2Points <= min)	{
+			min = p2Points;
+		}
+		if (p3Points <= min)	{
+			min = p3Points;
+		}
+		if (p4Points <= min)	{
+			min = p4Points;
+		}
+				
+		if (min == p1Points)
+		{
+			winner = 1;
+		}
+		else if (min == p2Points)
+		{
+			winner = 2;
+		}
+		else if (min == p3Points)
+		{
+			winner = 3;
+		}
+		else if (min == p4Points)
+		{
+			winner = 4;
+		}								
+	}		
 }
 
 void main(){
@@ -341,33 +647,78 @@ void main(){
 	printf("[+]Connected to P2\n");	
 	sendMessage("HelloP2", clientSocket2);
 	
-	while(!quitGame)
+	GAME:while(!quitGame && !waiting)
 	{
 		if (whoseTurn == 1)
-		{
-			printf("[ReceiveP1:]\n");
-			printf("%s\n",recMessage);
-			receiveMessage(clientSocket, whoseTurn);
+		{	
+			//printf("Rec2:%s  \n",recMessage);	
+			printf("[ReceiveP1:]\n");				
+			receiveMessage(clientSocket, whoseTurn);			
+			whoseTurn = 2;
 		}
 		else if (whoseTurn == 2)
-		{
-			printf("[ReceiveP2:]\n");
-			printf("%s",recMessage);
+		{	
+			//printf("Rec1:%s  \n",recMessage);		
+			printf("[ReceiveP2:]\n");						
 			receiveMessage(clientSocket2, whoseTurn);
+			whoseTurn = 4;
 		}					
 		else
 		{
 			printf("[SendP1:]\n");		
-			pickCard(3);
-			pickCard(4);					
-			buildTurnMessage(1,p1PickedCard,p2PickedCard,p3PickedCard,p4PickedCard);
-			sendMessage(message,clientSocket);
-			whoseTurn = 1;
+			pickCard(3);			
+			pickCard(4);			
+			printf("	[T] %d. kör: 1.:%d 2.:%d 3.:%d 4.:%d\n",turnCount-1, p1PickedCard, p2PickedCard, p3PickedCard, p4PickedCard);
+
+			if (turnCount > 0)
+			{
+				calculatePoints();
+				printf("		[S] %d. játékos kártyája az erősebb! 1.:%d 2.:%d 3.:%d 4.:%d\n",bestStrength(),s1,s2,s3,s4);
+				printf("			[P] Kör: %d pont.\n",tPoint);	
+
+				printf("%d\n",turnCount);
+				if (turnCount == 14)
+				{
+					bigTurnCount++;
+					printf("				[V]%d.kör eredménye: 1.:%d 2.:%d 3.:%d 4.:%d\n",bigTurnCount, p1Points, p2Points, p3Points, p4Points);
+					printf("				[V]Új kör következik!\n");
+					getWinner();
+					resetTurn();					
+					printf("		Új kör: 2.ra várok!\n");
+					receiveMessage(clientSocket2, whoseTurn);					
+				}		
+				else if (turnCount != 14 && turnCount != 0)
+				{
+					buildTurnMessage(1,p1PickedCard,p2PickedCard,p3PickedCard,p4PickedCard,p1Points,p2Points,p3Points,p4Points);
+					sendMessage(message,clientSocket);
+					whoseTurn = 1;
+				}					
+			}											
 		}
+	}
+
+	WAIT:while(waiting)
+	{
+		if (whoseTurn == 1)
+		{
+			receiveMessage(clientSocket, whoseTurn);
+		}
+		else
+		{
+			receiveMessage(clientSocket2, whoseTurn);
+		}
+
+		if (!waiting)
+		{
+			goto GAME;
+		}		
 	}
 
     printf("[+]Closing the connection.\n");
 
+	setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof on);
+	setsockopt(serverSocket, SOL_SOCKET, SO_KEEPALIVE, (char *)&on, sizeof on);
+
 	close(clientSocket);
-	close(serverSocket);
+	close(serverSocket);	
 }
